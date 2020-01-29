e7279b32e4cfb596ef8c30ba957d97e60ce98d20
Sandra Roques <roquesandra@hotmail.fr>
2020-01-29 10:54:27 +0100

    first commit

---
+++ src/main/java/com/wildcodeschool/WideWildCircus/WideWildCircusApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.WideWildCircus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WideWildCircusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WideWildCircusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/WideWildCircus/WideWildCircusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.WideWildCircus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WideWildCircusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
