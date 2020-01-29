0eff54751a7884b9c8e23008f07762943e1f9d8d
Sandra Roques <roquesandra@hotmail.fr>
2020-01-29 12:23:56 +0100

    add navbar

--- src/main/java/com/wildcodeschool/WideWildCircus/controllers/HomeController.java
+++ src/main/java/com/wildcodeschool/WideWildCircus/controllers/HomeController.java
@@ -2,27 +2,13 @@ package com.wildcodeschool.WideWildCircus.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
 		
 		@GetMapping("/")
-		@ResponseBody
 		public String home() {
-			return "Welcome to the SHIELD";
+			return "index";
 		}
-		
-		@GetMapping("/admin")
-		public String admin() {
-			return "redirect:/admin/dashboard";
-		}
-		
-		@GetMapping("/admin/dashboard")
-		public String champion() {
-			return "admin_home";
-		}
-
-
 }
