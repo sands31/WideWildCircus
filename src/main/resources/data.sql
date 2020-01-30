c232b3a9fc57ebb80b58b0aef208a4a102753aad
Sandra Roques <roquesandra@hotmail.fr>
2020-01-30 15:46:13 +0100

    comment layout done

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,8 @@ INSERT INTO `article` (`author`, `content`, `date`, `picture_path`, `publish_sta
 
 UPDATE `article`
 SET content = 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.' WHERE 1 = 1;
+
+
+
+INSERT INTO `comment` (`author`, `content`, `date`, `email`, `article_id`) VALUES ('Jean', 'De toute beauté !', '2020-01-30', 'jean@hotmail.fr', '1');
+INSERT INTO `comment` (`author`, `content`, `date`, `email`, `article_id`) VALUES ('Aymeric', 'Bof, ils avaient pas de technique, ils y allaient à la zob', '2020-01-30', 'aymeric@hotmail.fr', '1');
