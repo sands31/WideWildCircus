123d5f62575f52e1280984d7095b1160bbdb0b98
sands31 <49200520+sands31@users.noreply.github.com>
2020-09-10 15:30:55 +0200

    fixed csrf issue and added favicon.ico

--- src/main/java/com/wildcodeschool/WideWildCircus/config/MySecurityConfig.java
+++ src/main/java/com/wildcodeschool/WideWildCircus/config/MySecurityConfig.java
@@ -8,42 +8,42 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-		    http
-		    .sessionManagement()
-        	.sessionCreationPolicy(SessionCreationPolicy.STATELESS)	            
-	        .and()
-	        .authorizeRequests()
-	        .antMatchers("/", "/articles", "/css/**", "/img/**", "/js/**").permitAll()
-	        .antMatchers("/admin**").hasRole("ADMIN")
-	            .and()
-	        .formLogin()
-	        .defaultSuccessUrl("/admin/dashboard")
-	            .and()
-	        .httpBasic()
-		    	.and()
-			.logout()
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.authorizeRequests()
+				.antMatchers("/", "/articles", "/css/**", "/img/**", "/js/**", "/sound/**", "/favicon.ico").permitAll()
+				.antMatchers("/admin**").hasRole("ADMIN")
+				.and()
+				.formLogin()
+				.defaultSuccessUrl("/admin/dashboard")
+				.and()
+				.httpBasic()
+				.and()
+				.logout()
 				.invalidateHttpSession(true)
 				.logoutUrl("/logout")
 				.logoutSuccessUrl("/login")
-		    .and()
-			.sessionManagement()
-			.maximumSessions(1)
-			.expiredUrl("/login");
-		}
-		
-		@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		    auth
-	    		.inMemoryAuthentication()
-	    			.withUser("wildchief")
-		    			.password(encoder.encode("admin"))
-		    			.roles("ADMIN");
-		}
-} 
+				.and()
+				.sessionManagement()
+				.maximumSessions(1)
+				.expiredUrl("/login");
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth
+				.inMemoryAuthentication()
+				.withUser("wildchief")
+				.password(encoder.encode("admin"))
+				.roles("ADMIN");
+	}
+}
